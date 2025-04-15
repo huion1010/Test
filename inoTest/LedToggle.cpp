8c1ef71352358f03922e735485c028c300386090
huion1010 <59288135+huion1010@users.noreply.github.com>
2025-04-15 15:28:20 +0900

    Update LedToggle.cpp

--- inoTest/LedToggle.cpp
+++ inoTest/LedToggle.cpp
@@ -7,6 +7,14 @@ LedToggle::LedToggle(int pin) {
 	digitalWrite(_pin, LOW);
 }
 
+LedToggle::LedToggle(int pin, unsigned long delayTime) {
+	_pin = pin;
+	_state = false;
+	_delayTime = delay;
+	pinMode(_pin, OUTPUT);
+	digitalWrite(_pin, LOW);
+}
+
 void LedToggle::toggle() {
 	_state = !_state;
 	digitalWrite(_pin, _state ? HIGH : LOW);
